56d7dda73397f26d737cd8824e8b67a701d568ae
petermaringer <59333030+petermaringer@users.noreply.github.com>
2021-09-24 18:44:12 +0200

    Update ViewController.swift

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -1411,20 +1411,24 @@ webview.evaluateJavaScript("navigator.userAgent") { (result, error) in
     if restoreIndex == restoreIndexLast {
       restoreIndex += 1
       
-      let sessionRestorePath = Bundle.main.path(forResource: "SessionRestore", ofType: "html")
-      let sessionRestoreString = try? String(contentsOfFile: sessionRestorePath!, encoding: String.Encoding.utf8)
-      lb.text = lb.text! + " RDONE:\(sessionRestoreString)"
-      adjustLabel()
+      //let sessionRestorePath = Bundle.main.path(forResource: "SessionRestore", ofType: "html")
+      //let sessionRestoreString = try? String(contentsOfFile: sessionRestorePath!, encoding: String.Encoding.utf8)
+      //lb.text = lb.text! + " RDONE:\(sessionRestoreString)"
+      //adjustLabel()
       
       let webServer = GCDWebServer()
       webServer.addDefaultHandler(forMethod: "GET", request: GCDWebServerRequest.self, processBlock: {request in
         //return GCDWebServerDataResponse(html:"<html><body><p>Hello Worldo</p></body></html>")
-        return GCDWebServerDataResponse(html: sessionRestoreString!)
+        //return GCDWebServerDataResponse(html: sessionRestoreString!)
+        if let sessionRestorePath = Bundle.main.path(forResource: "SessionRestore.html", ofType: nil), let sessionRestoreString = try? String(contentsOfFile: sessionRestorePath, encoding: String.Encoding.utf8) {
+        lb.text = lb.text! + " RDONE:\(sessionRestoreString)"
+        adjustLabel()
+        return GCDWebServerDataResponse(html: sessionRestoreString)
         //guard let sessionRestorePath = Bundle.main.path(forResource: "SessionRestore", ofType: "html"), let sessionRestoreString = try? String(contentsOfFile: sessionRestorePath) else {
           //self.lb.text = self.lb.text! + "R404"
           //self.adjustLabel()
           //return GCDWebServerResponse(statusCode: 404)
-        //}
+        }
         //self.lb.text = self.lb.text! + "RDONE"
         //self.adjustLabel()
         //return GCDWebServerDataResponse(html: sessionRestoreString)
