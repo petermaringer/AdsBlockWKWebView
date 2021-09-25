82c4b07e0c6a3e55847bd85f0f76b3980dcabc46
petermaringer <59333030+petermaringer@users.noreply.github.com>
2021-09-25 08:06:39 +0200

    Update ViewController.swift

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -1449,7 +1449,7 @@ webview.evaluateJavaScript("navigator.userAgent") { (result, error) in
         return GCDWebServerDataResponse(html: sessionRestoreString)
       })
       
-      webServer.addGETHandlerForBasePath("/", directoryPath: Bundle.main.path(forResource: "/", ofType: nil)!, indexFilename: "adaway.json", cacheAge: 0, allowRangeRequests: true)
+      webServer.addGETHandler(forBasePath: "/", directoryPath: Bundle.main.path(forResource: "/", ofType: nil)!, indexFilename: "adaway.json", cacheAge: 0, allowRangeRequests: true)
       
       //webServer.start(withPort: 6571, bonjourName: "GCD Web Server")
       try? webServer.start(options: [GCDWebServerOption_Port: 6571, GCDWebServerOption_BindToLocalhost: true, GCDWebServerOption_AutomaticallySuspendInBackground: true])
