3f95f7e34e6235adbee3903d04352415104d9b6e
petermaringer <59333030+petermaringer@users.noreply.github.com>
2021-09-26 20:50:52 +0200

    Update ViewController.swift

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -84,7 +84,7 @@ class SessionRestoreHandler {
       if let range = request?.url.absoluteString.range(of: "=") {
         let phone = request?.url.absoluteString[range.upperBound...].removingPercentEncoding
         //return GCDWebServerDataResponse(html: "hi:\(phone!)")
-        return GCDWebServerDataResponse(redirect: URL(string: phone)!, permanent: false)
+        return GCDWebServerDataResponse(redirect: URL(string: phone!)!, permanent: false)
       }
       //return GCDWebServerDataResponse(html: "hi:error")
       return GCDWebServerResponse(statusCode: 404)
