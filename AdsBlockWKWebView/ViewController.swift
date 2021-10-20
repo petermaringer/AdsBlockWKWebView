2b71d214d0eee4b3867087bf720a2a63ccb6de7f
petermaringer <59333030+petermaringer@users.noreply.github.com>
2021-10-20 21:54:59 +0200

    Update ViewController.swift

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -98,7 +98,7 @@ class SessionRestoreHandler {
       if let range = request?.url.absoluteString.range(of: "=") {
         //let phone = request?.url.absoluteString[range.upperBound...].removingPercentEncoding!.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
         let phone1 = request?.url.absoluteString[range.upperBound...]
-        let phone = String(phone1!)
+        let phone = String(phone1)
         //return GCDWebServerDataResponse(html: "hi:\(phone!)")
         return GCDWebServerDataResponse(redirect: URL(string: phone!)!, permanent: false)
       }
