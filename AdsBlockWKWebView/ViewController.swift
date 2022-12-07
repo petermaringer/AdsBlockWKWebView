86799299b7843a08c0dd6515b7f8dd02837397f0
petermaringer <petermaringer@notmailinator.com>
2022-12-07 21:12:45 +0100

    b795

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -1391,9 +1391,11 @@ webview.evaluateJavaScript("navigator.userAgent") { (result, error) in
     guard let fileURL = urlOrNil else { return }
     do {
         let documentsURL = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-        let fileext = responseOrNil?.suggestedFilename
-        self.lb.text! += " \(fileext!)"
-        let savedURL = documentsURL.appendingPathComponent(fileURL.lastPathComponent)
+        
+        let savedURL = documentsURL.appendingPathComponent(responseOrNil!.suggestedFilename)
+        self.lb.text! += " \(savedURL)"
+        
+        //let savedURL = documentsURL.appendingPathComponent(fileURL.lastPathComponent)
         try FileManager.default.moveItem(at: fileURL, to: savedURL)
     } catch {
         //print ("file error: \(error)")
