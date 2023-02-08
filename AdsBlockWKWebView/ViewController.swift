a0c4a411cac7c15a1161837048e5d7c0bc55454b
petermaringer <petermaringer@notmailinator.com>
2023-02-08 23:43:27 +0100

    b838

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -789,7 +789,7 @@ player.play()*/
     let (publicKeyBits, potentialPublicKeyBlockSize) = getPublicKeyBits(keyAlgorithm, publicKey: publicKey!, tagPublic: tagPublic)
     
     let csr = CertificateSigningRequest(commonName: "Wolfgang Weinmann", countryName: "AT", emailAddress: "apps@weinmann.co.at", keyAlgorithm: keyAlgorithm)
-    let builtCSR = csr.buildCSRAndReturnString(publicKeyBits, privateKey: privateKey!)
+    let builtCSR = csr.buildCSRAndReturnString(publicKeyBits!, privateKey: privateKey!)
     showAlert(message: "CSR:\n\n\(builtCSR)")
     
     //https://github.com/digitalbazaar/forge
