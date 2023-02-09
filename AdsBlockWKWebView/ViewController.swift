b67f63a26ece0aeb9986d8c86f6aed645db0a9b7
petermaringer <petermaringer@notmailinator.com>
2023-02-09 02:33:09 +0100

    b846

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -788,7 +788,7 @@ player.play()*/
     let publicKeyBits = getPublicKeyBits(keyAlgorithm, publicKey: publicKey!, tagPublic: tagPublic)
     let csr = CertificateSigningRequest(commonName: "Wolfgang Weinmann", countryName: "AT", emailAddress: "apps@weinmann.co.at", keyAlgorithm: keyAlgorithm)
     let builtCSR = csr.buildCSRAndReturnString(publicKeyBits!, privateKey: privateKey!)
-    try! builtCSR.write(to: FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent("ssl.csr"), atomically: true, encoding: .utf8)
+    try! builtCSR!.write(to: FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent("ssl.csr"), atomically: true, encoding: .utf8)
     showAlert(message: "CSR:\n\n\(builtCSR!)")
     
     
