40f1fd2f2cab939831393d547a7b3986dd88c24e
petermaringer <petermaringer@notmailinator.com>
2023-02-22 16:35:45 +0100

    b998

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -2264,8 +2264,8 @@ downloadTask.resume()
       alertCounter += 1
       showJSAlert(type: "prompt", title: "Alert", message: prompt, input: defaultText) { (response) in
         //self.lb.text! += " RES:\(response!)/\(alertCounter)"
-        //completionHandler(response as String?)
-        completionHandler(nil)
+        completionHandler(response as String?)
+        //completionHandler(nil)
       }
     } else {
       completionHandler(nil)
