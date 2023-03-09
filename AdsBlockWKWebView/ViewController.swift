c5b4b3c1e9a7a3030f5778a6b7895ba692ebd7d3
petermaringer <petermaringer@notmailinator.com>
2023-03-09 19:27:48 +0100

    b1041

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -1741,10 +1741,10 @@ downloadTask.resume()
       var pageColor = "\(result ?? "")"
       pageColor = pageColor.components(separatedBy: CharacterSet(charactersIn: "rgba( )")).joined(separator: "")
       pageColor.components(separatedBy: ",")
-      let returnedColor = UIColor(r: Int(String(pageColor[0])), g: Int(pageColor[1]), b: Int(pageColor[2]), a: Int(pageColor[3]))
+      let returnedColor = UIColor(r: Int(String(pageColor[0]))!, g: Int(String(pageColor[1]))!, b: Int(String(pageColor[2]))!, a: Int(String(pageColor[3]))!)
       //UIColor(red: CGFloat(pageColor[0]) / 255.0, green: CGFloat(pageColor[1]) / 255.0, blue: CGFloat(pageColor[2]) / 255.0, alpha: CGFloat(pageColor[3]) / 255.0)
       topNavBgView.backgroundColor = returnedColor
-      self.lb.text! += " \(result ?? "nil") \(pageColor)"
+      self.lb.text! += " \(result ?? "nil") \(pageColor) \(pageColor[1])"
     }
     //topNavBgView.backgroundColor = .appBgLightColor
     
