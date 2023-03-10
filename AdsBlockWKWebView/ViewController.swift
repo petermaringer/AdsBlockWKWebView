5d7f813e5f64933a3fcc4bd7f5f53eadeff145d2
petermaringer <petermaringer@notmailinator.com>
2023-03-10 22:23:31 +0100

    b1046

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -1576,6 +1576,7 @@ webview.evaluateJavaScript("navigator.userAgent") { (result, error) in
     NotificationCenter.default.addObserver(self, selector: #selector(enterBackground), name: .UIApplicationDidEnterBackground, object: nil)
     NotificationCenter.default.addObserver(self, selector: #selector(enterForeground), name: .UIApplicationWillEnterForeground, object: nil)
     NotificationCenter.default.addObserver(self, selector: #selector(resignActive), name: .UIApplicationWillResignActive, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(willTerminate), name: .UIApplicationWillTerminate, object: nil)
     let commandCenter = MPRemoteCommandCenter.shared()
     commandCenter.togglePlayPauseCommand.addTarget { [unowned self] event in
       if self.avPVC.player!.rate == 0.0 {
@@ -1759,7 +1760,10 @@ downloadTask.resume()
       webview.scrollView.setContentOffset(CGPoint(x: webview.scrollView.contentOffset.x, y: 0), animated: true)
     }
     lb.text! += " rAc"
-    //adjustLabel()
+  }
+  
+  @objc private func willTerminate() {
+    lb.text! += " wTe"
   }
   
   
