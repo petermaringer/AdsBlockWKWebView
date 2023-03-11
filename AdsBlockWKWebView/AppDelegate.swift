1cdd47b5601a1854aa5a02cf94a53f0feefcadfc
petermaringer <petermaringer@notmailinator.com>
2023-03-11 20:47:13 +0100

    b1051

--- AdsBlockWKWebView/AppDelegate.swift
+++ AdsBlockWKWebView/AppDelegate.swift
@@ -11,6 +11,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var sesscat: String = "sesscat"
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    
+    var paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+let documentsDirectory = paths[0]
+let fileName = "\(Date()).log"
+let logFilePath = (documentsDirectory as NSString).appendingPathComponent(fileName)
+freopen(logFilePath.cString(using: String.Encoding.ascii)!, "a+", stderr)
+    
     self.window = UIWindow(frame: UIScreen.main.bounds)
     if let window = self.window {
       let viewController = ViewController()
