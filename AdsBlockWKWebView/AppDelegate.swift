d8208e8bd019fe14aaf6939b93aa529397073a81
petermaringer <petermaringer@notmailinator.com>
2023-03-11 16:31:11 +0100

    b1049

--- AdsBlockWKWebView/AppDelegate.swift
+++ AdsBlockWKWebView/AppDelegate.swift
@@ -6,64 +6,32 @@
 import UIKit
 
 //BackgroundAudioBegin
-import AVFoundation
-import AVKit
+//import AVFoundation
+//import AVKit
 //BackgroundAudioEnd
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-    var sesscat: String = "hi"
-    //let videoURL = URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8")
-    ///let player = AVPlayer(url: URL(string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8")!)
-    ///let playerViewController = AVPlayerViewController()
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        if let window = self.window {
-            let viewController = ViewController()
-            window.rootViewController = viewController
-            window.backgroundColor = UIColor.white
-            window.makeKeyAndVisible()
-        }
-        
-        /*
-        //BackgroundAudioBegin
-        let session = AVAudioSession.sharedInstance()
-        do {
-            try session.setCategory(AVAudioSessionCategoryPlayback)
-            //try session.setCategory(AVAudioSessionCategoryPlayback, with: .mixWithOthers)
-            try session.setActive(true)
-        } catch {
-            print(error.localizedDescription)
-        }
-        sesscat = "\(session.category)"
-        //BackgroundAudioEnd
-        */
-        
-        return true
+  var window: UIWindow?
+  var sesscat: String = "sesscat"
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    if let window = self.window {
+      let viewController = ViewController()
+      window.rootViewController = viewController
+      window.backgroundColor = UIColor.white
+      window.makeKeyAndVisible()
     }
-
-    ///func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        ///playerViewController.player = nil
-    ///}
-
-    ///func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        ///playerViewController.player = player
-    ///}
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
+    return true
+  }
+  
+    ///func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
+    ///}
 
-    func applicationWillTerminate(_ application: UIApplication) {
+    ///func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
+    ///}
 
 }
-
