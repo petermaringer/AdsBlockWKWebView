b5d289f9f39cb4f4d645b859134668c832e951c4
petermaringer <petermaringer@notmailinator.com>
2023-03-12 11:00:13 +0100

    b1057

--- AdsBlockWKWebView/AppDelegate.swift
+++ AdsBlockWKWebView/AppDelegate.swift
@@ -5,13 +5,6 @@
 
 import UIKit
 
-import Darwin
-import os.log
-extension OSLog {
-    private static var subsystem = Bundle.main.bundleIdentifier!
-    static let test = OSLog(subsystem: subsystem, category: "Test")
-}
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
@@ -19,39 +12,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     
-    /*
-    let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
-let documentsDirectory = paths[0]
-let fileName = "\(Date()).log"
-let logFilePath = (documentsDirectory as NSString).appendingPathComponent(fileName)
-freopen(logFilePath.cString(using: String.Encoding.ascii)!, "a+", stderr)
-freopen(logFilePath.cString(using: String.Encoding.ascii)!, "a+", stdout)
-    */
-    if true /*isatty(STDERR_FILENO) != 1*/ { // if no terminal is attached to stderr
-            let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-            let logfileUrl = documentsUrl.appendingPathComponent("out.log")
-            logfileUrl.withUnsafeFileSystemRepresentation { path in
-                guard let path = path else {
-                    return
-                }
-                print("redirect stderr and stderr to: \(String(cString: path))")
-                let file = fopen(path, "a")
-                assert(file != nil, String(cString: strerror(errno)))
-                let fd = fileno(file)
-                assert(fd >= 0, String(cString: strerror(errno)))
-                let result1 = dup2(fd, STDERR_FILENO)
-                assert(result1 >= 0, String(cString: strerror(errno)))
-                let result2 = dup2(fd, STDOUT_FILENO)
-                assert(result2 >= 0, String(cString: strerror(errno)))
-
-                os_log("***** os_log: Test", log: OSLog.test, type: .debug)
-                print("***** print: Test")
-                NSLog("***** NSLog: Test")
-            }
-        }
-        os_log("os_log: Test", log: OSLog.test, type: .debug)
-        print("print: Test")
-        NSLog("NSLog: Test")
+    let documentsDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+    let fileName = "\(Date()).log"
+    let logFilePath = (documentsDirectory as NSString).appendingPathComponent(fileName)
+    freopen(logFilePath.cString(using: String.Encoding.ascii)!, "a+", stderr)
+    freopen(logFilePath.cString(using: String.Encoding.ascii)!, "a+", stdout)
+    print("print: TestAD")
+    NSLog("NSLog: TestAD")
     
     self.window = UIWindow(frame: UIScreen.main.bounds)
     if let window = self.window {
