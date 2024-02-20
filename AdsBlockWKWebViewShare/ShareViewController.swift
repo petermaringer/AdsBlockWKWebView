66d6d18be3fda08f4a0178b9947d3141911a5ccf
petermaringer <petermaringer@notmailinator.com>
2024-02-20 23:39:22 +0100

    b1155

---
+++ AdsBlockWKWebViewShare/ShareViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+@objc(PrincipalClassName)
+class ShareViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // do something here
+    }
+}
