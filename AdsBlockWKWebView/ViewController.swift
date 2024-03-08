9b57534fdde18e883f7527b8e0af7e78f4f6073c
petermaringer <petermaringer@notmailinator.com>
2024-03-08 22:24:45 +0100

    b1303

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -1918,7 +1918,7 @@ downloadTask.resume()
     
     let cleanStart = webView.load(URLRequest(url: URL(string: "https://www.google.com/")!))
     //let restoreStart = webView.load(URLRequest(url: URL(string: "\(WebServer.instance.base)/errors/restore?history=\(restoreUrlsJson!)")!))
-    let restoreStart: () -> () = showAlert(message: "hey")
+    let restoreStart: () -> Void = showAlert(message: "hey")
     if webViewRestorePref == "never" {
       //cleanStart()
       //return
