c1d32d39883a9baf7ea8b4e4a454e5a7475b3582
petermaringer <petermaringer@notmailinator.com>
2024-03-10 17:13:55 +0100

    b1328

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -205,17 +205,13 @@ class SessionRestoreHandler {
       if let range = request?.url.absoluteString.range(of: "=") {
         let phoneTest1 = request?.url.absoluteString[range.upperBound...]
         NSLog("NSLog: pT0|\(phoneTest1!)")
-        var phoneTest = request?.url.absoluteString[range.upperBound...].replacingOccurrences(of: "%25", with: "%")
-        NSLog("NSLog: pT1|\(phoneTest!)")
-        //let phoneTestSub = phoneTest!.replacingOccurrences(of: "%25", with: "%")
-        //phoneTest = phoneTestSub!
-        //NSLog("NSLog: pT2|\(phoneTest!)")
+        //var phoneTest = request?.url.absoluteString[range.upperBound...].replacingOccurrences(of: "%25", with: "%")
+        //NSLog("NSLog: pT1|\(phoneTest!)")
         phoneTest = phoneTest!.removingPercentEncoding!
         NSLog("NSLog: pT2|\(phoneTest!)")
+        try! "\(phoneTest!)\n\n".write(to: URL.docDir.appendingPathComponent("debug2.txt"), atomically: true, encoding: .utf8)
         phoneTest = phoneTest!.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
         NSLog("NSLog: pT3|\(phoneTest!)")
-        //phoneTest = request?.url.absoluteString[range.upperBound...].removingPercentEncoding!
-        //NSLog("NSLog: pT|\(phoneTest!)")
         //let phone = request?.url.absoluteString[range.upperBound...].removingPercentEncoding!.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
         let phone = request?.url.absoluteString[range.upperBound...].removingPercentEncoding!
         NSLog("NSLog: \(phone!)")
