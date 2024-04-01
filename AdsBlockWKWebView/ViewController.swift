01be1570d360981dc839e4be260c2942cd67725c
petermaringer <petermaringer@notmailinator.com>
2024-04-01 21:16:30 +0200

    b1543

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -562,7 +562,7 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, WKSc
   
   @objc internal func onMenu1(sender: UIMenuItem) {
     
-    let testio = userDefGroup.getData(key: "testkey") as! Int
+    let testio: Int = userDefGroup.getData(key: "testkey") as! Int
     showAlert(message: "t: \(testio)")
     userDefGroup.setData(Int(arc4random_uniform(999999) + 1), key: "testkey")
     
