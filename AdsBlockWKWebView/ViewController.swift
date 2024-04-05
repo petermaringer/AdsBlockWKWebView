424dba71848ea5e8d5f10de8fd82fac38a88f9ea
petermaringer <petermaringer@notmailinator.com>
2024-04-05 10:38:27 +0200

    b1565

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -1439,13 +1439,12 @@ player.play()*/
   private func showNewAlert(type: String? = "alert", title: String? = nil, _ message: String? = nil, input: String? = nil, completionHandler: @escaping (Any?) -> Void = { _ in }) {
     if let message = message {
       alertObjArray.append(alertObj(type: type, title: title, message: message, input: input, completionHandler: completionHandler))
-      //...put) { res in completionHandler(res) })
     }
     guard alertObjArray.count > 0 else { return }
     let alert = UIAlertController(title: alertObjArray.first!.title, message: alertObjArray.first!.message, preferredStyle: .alert)
     if alertObjArray.first!.type == "alert" {
       alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
-        alertObjArray.first!.completionHandler("\(alertObjArray.count)/\(alertObjArray.first!.message!)")
+        alertObjArray.first!.completionHandler("\(alertObjArray.first!.message!)")
         alertObjArray.removeFirst()
         self.showNewAlert()
       }))
