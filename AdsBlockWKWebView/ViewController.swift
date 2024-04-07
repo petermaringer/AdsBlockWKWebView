d9c9d4bab204fb04a6e2b903d3a8b8c3d0455f63
petermaringer <petermaringer@notmailinator.com>
2024-04-08 00:47:56 +0200

    b1585

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -1453,8 +1453,8 @@ player.play()*/
     self.present(alert, animated: true) { hapticFB.notificationOccurred(.success) }
   }
   */
-  private func showNewAlert(type: String? = "alert", style: UIAlertController.Style? = .alert, title: String? = "Alert", _ message: String? = nil, input: String? = nil, completionHandler: @escaping (Any?, Any?) -> Void = { _(, _)* in }) {
-    //{ _ in } //{ _, _ in }
+  private func showNewAlert(type: String? = "alert", style: UIAlertController.Style? = .alert, title: String? = "Alert", _ message: String? = nil, input: String? = nil, completionHandler: @escaping (Any?, Any?) -> Void = { _, _ in }) {
+    //{ _ in }
     if message != "nextAlertObj" {
       alertObjArray.append(alertObj(type: type, style: style, title: title, message: message, input: input, completionHandler: completionHandler))
     }
