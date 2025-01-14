702acf201ea0878b3ffef61d4895c60147938844
petermaringer <petermaringer@notmailinator.com>
2025-01-14 14:35:00 +0100

    b1641

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -214,8 +214,9 @@ class HttpServer: NSObject {
     server.route(.GET, "status") { (.ok, "Server is running ö") }
     server.route(.GET, "auth//*") { (.unauthorized, "401 Unauthorized ö") }
     //server.route(.GET, "/*") { (.forbidden, "403 Forbidden ö") }
-    server.route(.GET, ".*test$") { (.forbidden, "403 Forbidden ö") }
-    server.route(.GET, "^(?!wallet).*") { (.forbidden, "403 Forbidden ö") }
+    server.route(.GET, ".*\.txt$") { (.forbidden, "403 Forbidden ö1") }
+    server.route(.GET, ".*test\/$") { (.forbidden, "403 Forbidden ö2") }
+    server.route(.GET, "^(?!/wallet).*") { (.forbidden, "403 Forbidden ö3") }
     //server.route(.GET, "wallet.html", handleAuth)
     //server.route(.POST,"/",handlePost)
     //server.route(.PUT,"/:name",handlePut)
