b48de3f9419a43783625e65e7ae0de49a9024475
petermaringer <petermaringer@notmailinator.com>
2025-02-26 10:22:04 +0100

    b1716

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -2526,12 +2526,12 @@ downloadTask.resume()
     /*var allowed = CharacterSet.alphanumerics
     allowed.insert(charactersIn: "-._~:/?#[]@!$&'()*+,;=%")
     url = url.addingPercentEncoding(withAllowedCharacters: allowed)*/
-    var urlobj = URL(string: url)
-    if urlobj?.scheme == nil {
-      urlobj = URL(string: "http://" + url)
+    var urlobj = URL(string: url)!
+    if urlobj.scheme == nil {
+      urlobj = URL(string: "http://" + url)!
     }
     
-    if var components = URLComponents(url: urlobj?, resolvingAgainstBaseURL: false) {
+    if var components = URLComponents(url: urlobj, resolvingAgainstBaseURL: false) {
       components.scheme = components.scheme?.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
       components.user = components.user?.addingPercentEncoding(withAllowedCharacters: .urlUserAllowed)
       components.password = components.password?.addingPercentEncoding(withAllowedCharacters: .urlPasswordAllowed)
@@ -2539,10 +2539,11 @@ downloadTask.resume()
       components.path = components.path.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? ""
       components.percentEncodedQuery = components.query?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
       components.fragment = components.fragment?.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed)
-      urlobj = URL(string: components.url?.absoluteString)
+      //urlobj = URL(string: components.url?.absoluteString)
+      urlobj = components.url!
     }
     
-    lb.text! += " sL:\(urlobj!.absoluteString)"
+    lb.text! += " sL:\(urlobj.absoluteString)"
     
     /*var allowed = CharacterSet.alphanumerics
     allowed.insert(charactersIn: "-._~:/?#[]@!$&'()*+,;=%")
@@ -2565,7 +2566,8 @@ downloadTask.resume()
       lb.text! += "|\(urlobj!.absoluteString)"
     }*/
     navTypeBackForward = false
-    let request = URLRequest(url: urlobj!, timeoutInterval: 10.0)
+    //let request = URLRequest(url: urlobj!, timeoutInterval: 10.0)
+    let request = URLRequest(url: urlobj, timeoutInterval: 10.0)
     webView.load(request)
   }
   
